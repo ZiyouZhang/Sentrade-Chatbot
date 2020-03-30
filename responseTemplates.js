f803ae85ce83d599077f235d5d4ff452c4590490
Ziyou Zhang <ziyou.zhang@outlook.com>
2020-03-30 17:46:43 +0800

    added more sample tweets

--- responseTemplates.js
+++ responseTemplates.js
@@ -13,7 +13,7 @@ let news = [
     {
         "created_at": "Wed May 01 21:13:54 +0000 2019",
         "source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
-        "original_text": "RT @ovieali: American company Microsoft,has announced plans for the opening of its first Africa's engineering station in Nigeria. This come…",
+        "original_text": "RT @ovieali: American company Microsoft,has announced plans for the opening of its first Africa's engineering station in Nigeria.",
         "processed_text": "american company microsofthas announced plans for the opening of its first africas engineering station in nigeria this come",
         "date": "2019-05-01",
         "polarity": 0.125,
@@ -24,6 +24,33 @@ let news = [
         "source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
         "original_text": "I don't like this inconsistency in icons... 😅#microsofteams #OCD https://t.co/FsFGahsn0a",
         "processed_text": "i dont like this inconsistency in icons microsofteams ocd ",
+        "date": "2019-05-01",
+        "polarity": 0,
+        "subjectivity": 0
+    },
+    {
+        "created_at": "Wed May 01 19:54:40 +0000 2019",
+        "source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+        "original_text": "Excellent command of Microsoft kit; specially Excel, word and powerpoint. https://t.co/MqKcEXW5Bx",
+        "processed_text": " excellent command of microsoft kit specially excel word and powerpoint",
+        "date": "2019-05-01",
+        "polarity":0.5357142857142857,
+        "subjectivity": 0.6904761904761904
+    },
+    {
+        "created_at": "Wed May 01 14:36:40 +0000 2019",
+        "source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+        "original_text": "@Blockchain_Jay After Steve's death apple's innovation is gone. Microsoft takes over.",
+        "processed_text": "blockchainjay after steves death apples innovation is gone microsoft takes over",
+        "date": "2019-05-01",
+        "polarity": 0,
+        "subjectivity": 0
+    },
+    {
+        "created_at": "Wed May 01 21:44:02 +0000 2019",
+        "source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+        "original_text": "Microsoft announces the $3,500 HoloLens 2 Development Edition https://t.co/7KmJhENdNL",
+        "processed_text": "microsoft announces the 3500 hololens 2 development edition ",
         "date": "2019-05-01",
         "polarity": 0,
         "subjectivity": 0
@@ -45,7 +72,7 @@ function sendQuestion(news_number) {
             "type": "template",
             "payload": {
                 "template_type": "button",
-                "text": "The following tweet:\n\n\"" + news[news_number]["original_text"] + "\"\n\n has been identified as " + sentiment + ". Do you think it's correct?", 
+                "text": "The following tweet:\n\n\"" + news[news_number]["original_text"] + "\"\n\nhas been identified as " + sentiment + ". Do you think it's correct?", 
                 "buttons": [
                     {
                         "type": "postback",
